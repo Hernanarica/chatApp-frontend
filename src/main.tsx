73819336ed1769e14717e258143d37ddecf35bd2
Hernán Arica <hernodev@gmail.com>
2023-05-13 04:43:39 -0300

    Se inicializa el proyecto con las tecnologias y configuraciones iniciales

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './assets/css/tailwindcss.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h2>Hola mundo</h2>
+  </React.StrictMode>,
+);
