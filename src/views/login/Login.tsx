1987835546e69a5bb38b7d2aba1de2025e8b7245
hernan arica <hernanaricammm@gmail.com>
2023-05-13 21:15:19 -0300

    se agregan furmularios de login y registro

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,8 +1,25 @@
 export function Login() {
 
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <form>
+      <h2 className="text-2xl">Formulario de Login</h2>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          placeholder="Your email"
+          id="email"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Your password"
+          id="password"
+        />
+      </div>
+      <button className="bg-purple-600 text-white px-2 py-1 rounded">SignIn</button>
+    </form>
   );
 }
--- src/views/register/Register.tsx
+++ src/views/register/Register.tsx
@@ -1,8 +1,40 @@
 export function Register() {
-
   return (
-    <>
-      <h1>Register</h1>
-    </>
+    <form>
+      <h2 className="text-2xl">Formulario de registro</h2>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          placeholder="Your name"
+          id="name"
+        />
+      </div>
+      <div>
+        <label htmlFor="lastname">Lastname</label>
+        <input
+          type="text"
+          placeholder="Your lastname"
+          id="lastname"
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          placeholder="Your email"
+          id="email"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Your password"
+          id="password"
+        />
+      </div>
+      <button className="bg-purple-600 text-white px-2 py-1 rounded">Register</button>
+    </form>
   );
 }
